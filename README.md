90d0f65ee8480728a6a08c4b95b3cf3f85198679
volatale <nathanamarirose@gmail.com>
2025-08-20 20:07:08 +0100

    Update README.md, add dev dependencies

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+# Leetcode-All
+
 A repo used to track my progress through every LC problem in Python.
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from leetcode-all!")
-
-
-if __name__ == "__main__":
-    main()
